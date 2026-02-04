d007ea470978997650f2f293eceeb0361d1ce30b
Thibaut Nijs <contact@thibautnijs.be>
2026-02-04 17:02:25 +0100

    Removed unused middleware model

--- lib/models/request-middleware.ts
+++
@@ -1,8 +0,0 @@
-
-import type { MaybePromise } from 'bun';
-
-import type { BunifyRequest } from './../request';
-import type { BunifyResponse } from '../response';
-
-
-export type RequestMiddleware = (request: BunifyRequest) => MaybePromise<BunifyResponse | undefined>
