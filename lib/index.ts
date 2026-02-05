60ad39e4622019711596b2ab7b9c81f288912d7a
Thibaut Nijs <contact@thibautnijs.be>
2026-02-05 08:58:50 +0100

    Export all models and RequestLifecycle

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,18 @@
-export { BunifyRequest } from './request';
+/**
+ * 
+ * Only export the public classes, interfaces and types to prevent bloat
+ * 
+ */
+
+export { BunifyRequest, RequestLifecycle } from './request';
 export { BunifyResponse } from './response';
 export { Bunify } from './bunify'
 
 export type { BunifyOptions } from './options';
 
-export { defaultRequestIdGeneratorFactory, requestUUIDGeneratorFactory } from './utils/request-id'
+export {
+  defaultRequestIdGeneratorFactory,
+  requestUUIDGeneratorFactory
+} from './utils/request-id'
+
+export * from './models'
