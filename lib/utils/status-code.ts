7a2bc57faf53b9bbf36fe53157287f1617f2e46f
Thibaut Nijs <contact@thibautnijs.be>
2026-02-05 08:56:50 +0100

    Add status code validator + tests

--- lib/utils/status-code.ts
+++ lib/utils/status-code.ts
@@ -4,3 +4,12 @@ export function validateStatusCode(statusCode: number) {
   if (statusCode < 100 || statusCode > 1000)
     throw BUNIFY_ERR_RESPONSE_STATUS_CODE_INVALID
 }
+
+export function validateRedirectStatusCode(statusCode: number) {
+  if (statusCode < 300 || statusCode > 308)
+    throw BUNIFY_ERR_RESPONSE_STATUS_CODE_INVALID
+}
+
+export function isValidRedirectStatusCode(statusCode: number) {
+  return statusCode > 299 && statusCode < 309
+}
