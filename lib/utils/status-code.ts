fc2f87b3e8f174fcc3dd5b38d066e2e02a797ab4
Thibaut Nijs <contact@thibautnijs.be>
2026-02-09 11:39:07 +0100

    Correctly check redirect codes + test

--- lib/utils/status-code.ts
+++ lib/utils/status-code.ts
@@ -1,15 +1,27 @@
 import { BUNIFY_ERR_RESPONSE_STATUS_CODE_INVALID } from "../errors"
 
+const REDIRECT_VALID_STATUSCODES: Record<number, boolean> = {
+  300: true,
+  301: true,
+  302: true,
+  303: true,
+  304: true,
+  305: false, // Deprecated
+  306: false, // Reserved, unused
+  307: true,
+  308: true
+}
+
 export function validateStatusCode(statusCode: number) {
-  if (statusCode < 100 || statusCode > 1000)
+  if (statusCode < 100 || statusCode > 999)
     throw BUNIFY_ERR_RESPONSE_STATUS_CODE_INVALID
 }
 
 export function validateRedirectStatusCode(statusCode: number) {
-  if (statusCode < 300 || statusCode > 308)
+  if (!REDIRECT_VALID_STATUSCODES[statusCode])
     throw BUNIFY_ERR_RESPONSE_STATUS_CODE_INVALID
 }
 
 export function isValidRedirectStatusCode(statusCode: number) {
-  return statusCode > 299 && statusCode < 309
+  return REDIRECT_VALID_STATUSCODES[statusCode] ?? false
 }
--- test/utils/status-code.test.ts
+++ test/utils/status-code.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'bun:test';
-import { validateStatusCode } from '../../lib/utils/status-code';
+import { isValidRedirectStatusCode, validateRedirectStatusCode, validateStatusCode } from '../../lib/utils/status-code';
 
 
 describe('Status code', () => {
@@ -8,5 +8,24 @@ describe('Status code', () => {
       (statusCode) =>
         expect(() => validateStatusCode(statusCode))
           .toThrowError('Invalid statuscode was provided'))
+  })
+
+  describe('validateRedirectStatusCode', () => {
+    test.each([0, 200, 204, 299, 309, 400, 404, 500])('should throw error if statusCode is below 300 or above 308', (statusCode) => {
+      expect(() => validateRedirectStatusCode(statusCode))
+        .toThrowError('Invalid statuscode was provided')
+    })
+  })
+
+  describe('isValidRedirectStatusCode', () => {
+    test.each([0, 200, 204, 299, 309, 306, 305, 400, 404, 500])('should return false if statusCode is invalid', (statusCode) =>
+      expect(isValidRedirectStatusCode(statusCode))
+        .toBeFalse()
+    )
+
+    test.each([300, 301, 302, 303, 304, 307, 308])('should return true if statusCode is a valid redirect code', (statusCode) =>
+      expect(isValidRedirectStatusCode(statusCode))
+        .toBeTrue()
+    )
   })
 })
