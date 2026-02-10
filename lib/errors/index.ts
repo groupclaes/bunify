5cb812df4d3ec026a336693ca163556d82f4abb3
Thibaut Nijs <contact@thibautnijs.be>
2026-02-10 12:07:01 +0100

    Create new generic static errors

--- lib/errors/index.ts
+++ lib/errors/index.ts
@@ -11,8 +11,9 @@ export const BUNIFY_ERR_ALREADY_RUNNING = new BunifyError('Server is already run
 export const BUNIFY_ERR_NOT_RUNNING = new BunifyError('Server isn\'t running!')
 export const BUNIFY_ERR_DESC_HOOK_NOT_FOUND = new BunifyError('The provided hook is invalid')
 export const BUNIFY_ERR_DESC_ROUTE_EXISTS = new BunifyError('Route already exists, use override to replace')
+export const BUNIFY_ERR_DESC_ROUTE_MUST_START_WITH_SLASH = new BunifyError('All routes must start with a prefix slash!')
 
-
+export const BUNIFY_ERR_REQUEST_INVALID_REQID_HANDLER = new BunifyError('No correct requestId handler has been configured!')
 export const BUNIFY_ERR_REQUEST_HOOK_RETURNS_RESPONSE = new BunifyError('Hooks cannot just return a response. Use the route\'s handler instead')
 // export const BUNIFY_ERR_DESC_ROUTE_EXISTS = new BunifyError('Cannot override route without override being true')
 
